eb892ed5aa3b64a25648ac6d57fabef3ca986736
Adi-111 <Chandradityasingh11@gmail.com>
2024-09-06 18:17:56 +0530

    Signed-off-by: Adi-111 <Chandradityasingh11@gmail.com>

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = async () => {
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <Image src={logoImage} alt="alt" width={24} height={24} />
-            <p className="font-bold text-xl">Attire Arena</p>
+            <p className="font-bold text-xlv ">Attire Arena</p>
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -8,17 +8,17 @@ const Billboard: React.FC<BillboardProps> = ({
   data
 }) => {
   console
-  return ( 
+  return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-      <div style={{ backgroundImage: `url(${data?.imageUrl})` }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
+      <div style={{ backgroundImage: `url(${data?.imageUrl})` }} className="rounded-xl relative aspect-square md:aspect-[2.1/1] overflow-hidden bg-cover">
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+          <div className="font-bold font-[Timeless] text-white text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs absolute top-6 left-6 text-left">
             {data.label}
           </div>
         </div>
       </div>
     </div>
-   );
+  );
 };
 
 export default Billboard;
